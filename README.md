7e36baf916388a93693063257e53d2d84b218b4e
Geeta Vinnakota <geetavin464@gmail.com>
2012-10-13 16:17:54 -0400

    p2

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ programming_problems
 Solving Common Programming Problems in Ruby
 
 #1 Find the first unique element in an array
+#2 Reverse a string in place
+#3 
---
+++ p2.rb
@@ -0,0 +1,15 @@
+# Reverse a String in Place
+
+def reverse_string(str)
+l = str.length
+
+     for i in 0..l/2
+        temp = str[i]
+        str[i] = str[l-i-1]
+        str[l-i-1] = temp
+     end
+     str
+end
+
+
+reverse_string("string1")
