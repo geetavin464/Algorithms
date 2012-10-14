97d75434ed2a00f885c7e191d69ea80d7d6f42ec
Geeta Vinnakota <geetavin464@gmail.com>
2012-10-13 23:42:02 -0400

    updated

---
+++ Recursion_patterns/r1.rb
@@ -0,0 +1,80 @@
+# How to measure efficiency of recursive programs
+# Compare the efficiency of iterative solution vs recursive solution
+# Various Recursive Patterns
+# Recursion (Solve the Problem by finding solutions to simpler problems - Decrease & Conquer)
+# Mathematical Functions
+
+# Factorial (Recursive)
+
+def factorial(n)
+    case n
+    when 0            #Base Case. No longer calls itself
+        return 1
+    else              # Recursive Case
+        return n*factorial(n-1) 
+    end
+end
+
+factorial(5)
+
+# Factorial (Iterative)
+
+def factorial(n)
+   factorial = 1
+   while(n>0)
+       factorial = factorial*n
+       n-=1
+   end 
+    factorial
+end
+
+factorial(5)
+
+# GCD (Recursive)
+
+def gcd(a,b)
+    if(a>b and a%b ==0)
+        return b
+    else
+        gcd(a, a%b)
+    end
+end
+
+gcd(22,21)
+
+# GCD ( Iterative )
+
+def gcd(a,b)
+    
+end
+
+# Fibonacci (Recursive)
+
+def fibonacci(n)
+   case n
+   when 0         #Base Case 1
+       return 0
+   when 1         #Base Case 2    
+       return 1 
+   else
+      fibonacci(n-1)+fibonacci(n-2) 
+   end
+end
+
+fibonacci(7)
+# Fibonacci (Iterative)
+
+def fibonacci(n)
+   fib_a = 0
+   fib_b = 1
+   
+   for i in 2..n
+       fib = fib_a + fib_b
+       fib_a =  fib_b
+       fib_b = fib
+   end
+   
+   fib
+end
+
+fibonacci(7)
