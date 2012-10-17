45dd68d038ec4e8abb1f2857f5e5a6f49de943bf
Geeta Vinnakota <geetavin464@gmail.com>
2012-10-17 01:44:48 -0400

    add to dictionary

--- problems/p6.rb
+++ problems/p6.rb
@@ -1,5 +1,5 @@
 
-$dictionary = ["he", "hell","hello", "loan","a", "an", "any", "one", "anyone", "net", "the", "her", "there", "here"]
+$dictionary = ["he", "hell","hello", "loan","a", "an", "any", "on", "one", "anyone", "net", "the", "her", "there", "here"]
 
 def cap(str)
     cap_str = ""                                        # Initialize return value
